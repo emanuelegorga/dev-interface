82ede990cbb353c14abd5abaf6078778f71ac67d
Emanuele Gorga <emanuele@bitpesa.co>
2020-01-14 11:12:01 +0000

    Fetching articles on form page

--- src/components/Article/form.jsx
+++ src/components/Article/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import ListArticle from './ListArticle';
 const API_URL = 'http://localhost:3000/articles';
 
 class Form extends Component {
@@ -51,10 +52,10 @@ class Form extends Component {
       const newArticle = await axios.post(
         API_URL, prepareArticle, headerConfig
       )
-      .then((response) => {
-        console.log(response.data)
-        alert('Article created!')
-      })
+        .then((response) => {
+          console.log(response.data)
+          alert('Article created!')
+        })
     } catch (e) {
       console.error(e)
       alert('Error! The article has not been created.')
@@ -62,40 +63,45 @@ class Form extends Component {
   }
 
   render() {
-    const { title, body, author } = this.state;
+    const { title, body, author, token } = this.state;
 
     return (
-      <div className="container">
-        <div className="row pt5">
-          <div className="col-12 col-lg-6 offset-lg-3">
-            <h1 className="text-center">DevBlog</h1>
-          </div>
-          <div className="col-12 col-lg-6 offset-lg-3">
-            <input
-              onChange={(ev) => this.handleChangeField('title', ev)}
-              value={title}
-              placeholder="Article Title"
-              className="form-control my-3" />
-            <textarea
-              onChange={(ev) => this.handleChangeField('body', ev)}
-              value={body}
-              placeholder="Article Description"
-              className="form-control my-3">
-            </textarea>
-            <input
-              onChange={(ev) => this.handleChangeField('author', ev)}
-              value={author}
-              className="form-control my-3"
-              placeholder="Article Author" />
-            <button
-              onClick={this.handleSubmit}
-              className="btn btn-primary float-right">
-              Submit
-            </button>
-            <NavLink to="/"> Go Back </NavLink>
+      <>
+        <div className="container">
+          <div className="row pt5">
+            <div className="col-12 col-lg-6 offset-lg-3">
+              <h1 className="text-center">DevBlog</h1>
+            </div>
+            <div className="col-12 col-lg-6 offset-lg-3">
+              <input
+                onChange={(ev) => this.handleChangeField('title', ev)}
+                value={title}
+                placeholder="Article Title"
+                className="form-control my-3" />
+              <textarea
+                onChange={(ev) => this.handleChangeField('body', ev)}
+                value={body}
+                placeholder="Article Description"
+                className="form-control my-3">
+              </textarea>
+              <input
+                onChange={(ev) => this.handleChangeField('author', ev)}
+                value={author}
+                className="form-control my-3"
+                placeholder="Article Author" />
+              <button
+                onClick={this.handleSubmit}
+                className="btn btn-primary float-right">
+                Submit
+              </button>
+              <NavLink to="/"> Go Back </NavLink>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="row pt-5">
+          <ListArticle isFetching={true} token={token} />
+        </div>
+      </>
     );
   }
 }
